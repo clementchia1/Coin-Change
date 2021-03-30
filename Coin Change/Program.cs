8fa02f64b873ea1f1edfca49c4cb3f47f6cf3ebc
lemon <chiachingman1@gmail.com>
2021-03-30 23:15:47 +1000

    Add project files.

---
+++ Coin Change/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Coin_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coins = new int[] {1};
+            int amount = 0;
+            Console.WriteLine("result:" + CoinChange(coins,amount));
+
+
+        }
+        public static int CoinChange(int[] coins, int amount)
+        {
+            Array.Sort(coins);
+            int ptn = coins.Length - 1;
+            int counter =0;
+            while (ptn>=0)
+            {
+               
+                if (amount>0)
+                {
+                    if (amount > coins[ptn])
+                    {
+                        amount -= coins[ptn];
+                        counter++;
+                    }
+                    else if (amount - coins[ptn] == 0)
+                    {
+                        counter++;
+                        return counter;
+                    }
+                    
+                    else
+                    {
+                        ptn--;
+                    }
+                }
+                else if (amount - coins[ptn] <= 0)
+                {
+                    return amount;
+                }
+            }
+            return -1;
+
+        }
+    }
+}
